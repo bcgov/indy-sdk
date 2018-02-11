9ea11c35c6ed859bf3a322fb99d37f7da0361e82
Ian Costanzo <ian@anon-solutions.ca>
2018-02-11 08:18:12 -0800

    Simple performance test of multiple Alice transcripts and proofs

---
+++ samples/python/src/perf_main.py
@@ -0,0 +1,16 @@
+import asyncio
+import time
+
+from src import anoncreds, crypto, ledger, proof_performance_test
+
+
+async def main():
+    await anoncreds.demo()
+    await crypto.demo()
+    await ledger.demo()
+    await proof_performance_test.run()
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    time.sleep(1)  # FIXME waiting for libindy thread complete
