db65b45397024f272c43817a9545f8d249f94971
Ian Costanzo <ian@middleware360.com>
2018-03-15 21:44:06 -0700

    Updates

---
+++ doc/wallet/README.md
@@ -0,0 +1,14 @@
+
+# Enterprise Wallet - Overview
+
+## Proposed Design and Implementation
+
+## Rust SDK Code
+
+## Python SDK Code
+
+## TheOrgBook and Permitify Integration
+
+## Other Testing Tools
+
+## Performance Testing Overview
--- samples/rest-wallet/rest_server/settings.py
+++ samples/rest-wallet/rest_server/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '&@b+1a$y_+hxgd(3f_vn00fbq#lh8qwy^(nv6&02+3l4(1#0-g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
